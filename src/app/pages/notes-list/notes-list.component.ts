ff039fe0377b3d0b8b3bacdb2223d6453e924c11
mattishii26 <chihiro2657@gmail.com>
2020-04-03 13:59:02 -0700

    Animation effects
    Implement Angular Animations into the UI

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,11 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../../shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      //Animation entry
+      transition('void => *',[ //not existing (void) to any state (*)
+        //Initial State
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)', 
+          'margin-bottom':0,
+          //we have to expand out the padding properties
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        //we first want to animate the spacing (which includes height and margin)
+        animate('50ms', style({
+          height:'*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingRight: '*',
+          paddingLeft: '*',
+        })),
+        animate(100)
+      ]),
+      transition('* => void', [ //anything to nothing
+        //first scale up
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        //then scale down to normal size while beginning to fade out
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        // scale down and face out completely
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0,
+        })),
+        //then animate teh spacing (which includes height, margin and padding)
+        animate('150ms ease-out', style({
+          opacity: 0,
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+          'margin-bottom': 0,
+        })),
+      ]) 
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [ //psuedo selector for entering in
+          style({
+            opacity: 0,
+            height: 0
+          }),
+          stagger(100, [ //delay in animation 
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
